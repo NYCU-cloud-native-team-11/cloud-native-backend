fdeccd97c5a36e2f68b0e4f9a07c537a736c9278
kn71026 <nicolechou9712@gmail.com>
2022-06-01 21:34:06 +0800

    create POST /api/trends test

--- tests/api.test.js
+++ tests/api.test.js
@@ -38,3 +38,46 @@ describe('GET /api/trends/keywords/<company>', () => {
             .expect(200, done);
     });
 });
+
+
+// Test for POST /api/trends
+describe('POST /api/trends', () => {
+    const payload = {
+        company: 'TSMC',
+        count: 10,
+        date: '2022-05-20',
+    };
+    test('should store a new trend', (done) => {
+        agent
+            .post('/api/trends')
+            .send(payload)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test('should return 400 status', (done) => {
+        agent
+            .post('/api/trends')
+            .send({ company: 'TSMC' })
+            .set('Accept', 'application/json')
+            .expect(400)
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test('should return 400 status', (done) => {
+        agent
+            .post('/api/trends')
+            .send({ company: 'TSMC', count: 10 })
+            .set('Accept', 'application/json')
+            .expect(400)
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+});
