f50d833fa8f1f75b8e72f0e34d8c304ada80753d
kn71026 <nicolechou9712@gmail.com>
2022-06-02 15:10:39 +0800

    create POST /api/trends
    update post route

--- controllers/trends.js
+++ controllers/trends.js
@@ -9,3 +9,11 @@ module.exports.get_params = async (req, res) => {
     var trends = await Trend.find({ company: req.params.company }).exec();
     res.json(trends);
 }
+
+module.exports.post = async (req, res) => {
+    const trends = req.body;
+    const { company, count, date } = req.body;
+    const trend = new Trend({ company, count, date });
+    await trend.save();
+    res.sendStatus(200);
+}
